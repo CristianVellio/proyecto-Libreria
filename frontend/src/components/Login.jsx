d31a4e56f97557ce0ed53f8790380e2c26d046f4
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-11-30 17:17:02 -0300

    Secciones Login y Registro

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import { Link } from "react-router";
 import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [mensaje, setMensaje] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  const handleGoogleLogin = () => {};
+
   return (
     <div className="h-[calc(100vh-120px)] flex justify-center items-center">
       <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-xl font-semibold mb-4">Iniciar Sesion</h2>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -18,6 +30,7 @@ const Login = () => {
               Email
             </label>
             <input
+              {...register("email", { required: true })}
               type="email"
               name="email"
               id="email"
@@ -33,6 +46,7 @@ const Login = () => {
               Contraseña
             </label>
             <input
+              {...register("password", { required: true })}
               type="password"
               name="contrasena"
               id="password"
@@ -56,7 +70,10 @@ const Login = () => {
           </Link>
         </p>
         <div className="mt-4">
-          <button className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+          <button
+            onClick={handleGoogleLogin}
+            className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          >
             <FaGoogle className="mr-2" />
             Ingresa con Google
           </button>
