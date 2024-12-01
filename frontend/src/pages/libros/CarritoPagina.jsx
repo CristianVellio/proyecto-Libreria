cc7d892dba7af8b779e8826c11083201d47d170c
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-11-30 23:02:47 -0300

    Pagar

--- frontend/src/pages/libros/CarritoPagina.jsx
+++ frontend/src/pages/libros/CarritoPagina.jsx
@@ -104,7 +104,7 @@ const CarritoPagina = () => {
               to="/checkout"
               className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
-              Comprar
+              Pagar
             </Link>
           </div>
           <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
