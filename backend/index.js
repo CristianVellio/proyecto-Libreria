0b1d614bfacd64bf92514c108309fc8285605f00
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-05 04:39:32 -0300

    metodos

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://proyecto-libreria.vercel.app/"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
