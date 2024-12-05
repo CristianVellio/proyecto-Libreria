9d57427b1e35e2babddf53e24e42322407aa0f71
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-05 06:25:00 -0300

    casca

--- frontend/src/utils/getImgUrl.js
+++ frontend/src/utils/getImgUrl.js
@@ -1,6 +1,5 @@
-const getImgUrl = (name) => {
-  const baseUrl = "https://proyecto-libreria.vercel.app/assets/libros/"; // Cambia a tu URL base
-  return `${baseUrl}${name}`;
-};
+function getImgUrl(name) {
+  return new URL(`../assets/libros/${name}`, import.meta.url);
+}
 
 export { getImgUrl };
