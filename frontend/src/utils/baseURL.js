c426219f6dea01c615beced32a8e4908c0b5d8fc
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-05 05:29:23 -0300

    argarg

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseURL = () => {
-  return window._env_.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  return process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 };
 
 export default getBaseURL;
